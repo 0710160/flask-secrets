583c6476a404f6332503f71edcef24bf6a1c3c84
U-SCOLOUR\mattt <mattt@MATT-PC.internal.scolour.co.nz>
2021-10-06 13:46:14 +1300

    Updated template inheritence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
+from flask_bootstrap import Bootstrap
 
 
 class MyForm(FlaskForm):
@@ -12,6 +13,7 @@ class MyForm(FlaskForm):
 
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = "some-secret-key"
 
 
@@ -19,12 +21,16 @@ app.secret_key = "some-secret-key"
 def home():
     return render_template('index.html')
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = MyForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+            if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+                return render_template('success.html')
+            else:
+                return render_template("denied.html")
     return render_template('login.html', form=login_form)
-
 
 
 if __name__ == '__main__':
